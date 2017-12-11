d66974cdd7300a59170c35d395091b82e3476c1b
Warren Seymour <warren@fountainhead.tech>
2017-12-11 14:47:50 +0000

    Expose HTTPError class from index

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Resource } from './Resource';
 export { Link } from './Link';
 export { Form } from './Form';
 export { fetchAll } from './fetchAll';
+export { HTTPError } from './handleResponse';
